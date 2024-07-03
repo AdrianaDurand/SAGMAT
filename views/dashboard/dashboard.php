aaef47861f5e7d14110ea4ce7477c8b68c73a462
AdrianaDurand <adrianadurand828@gmail.com>
2024-07-02 23:49:02 -0500

    Colors  Dashboard

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -166,16 +166,18 @@
               label: "Estado de recursos tecnológicos",
               data: dataValues,
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)', // Color para 'Disponible'
+                'rgba(75, 192, 192, 0.2)', // Color para 'Disponible'
                 'rgba(54, 162, 235, 0.2)', // Color para 'Prestado'
                 'rgba(255, 206, 86, 0.2)', // Color para 'Mantenimiento'
-                'rgba(75, 192, 192, 0.2)' // Color para 'Baja'
+                'rgba(255, 99, 132, 0.2)' // Color para 'Bajas'
+
               ],
               borderColor: [
-                'rgba(255, 99, 132, 1)',
+                'rgba(75, 192, 192, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
+                'rgba(255, 99, 132, 1)',
+
               ],
               borderWidth: 2
             }]
